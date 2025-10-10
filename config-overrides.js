1c4691627a026293dfe3bc2b808fc7bed45b16fe
Tinz1232311 <nguyentin8704@gmail.com>
2025-10-10 15:47:56 +0700

    second commit

---
+++ config-overrides.js
@@ -0,0 +1,7 @@
+// CommonJS
+const { override, useBabelRc } = require("customize-cra");
+
+module.exports = override(
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useBabelRc()
+);
